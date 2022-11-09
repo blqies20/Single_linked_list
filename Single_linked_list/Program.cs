412e885e3dcbe44d724e68f6c987f1d4bbe9a2ba
ACER <ACER@DESKTOP-QH5VR0V>
2022-11-09 17:39:01 +0700

    add method transverse

--- Single_linked_list/Program.cs
+++ Single_linked_list/Program.cs
@@ -73,5 +73,34 @@ namespace single_linked_list
                 START = START.next;
             return true;
         }
+        /*Method untuk menge-check apakah node yang dimaksud ada didalam list*/
+        public bool Search(int nim, ref Node previous, ref Node current)
+        {
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim >= current.noMhs))
+            {
+                previous = current;
+                current = current.next;
+            }
+            if (current == null)
+                return (false);
+            else
+                return (true);
+        }
+        /*Method untuk Transverse/mengunjungi dan membaca isi list*/
+        public void transverse()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList kosong. \n");
+            else
+            {
+                Console.WriteLine("\nData didalam list adalah : \n");
+                Node currentNode;
+                for (currentNode = START; currentNode != null; currentNode = currentNode.next)
+                    Console.Write(currentNode.noMhs + " " + currentNode.nama + "\n");
+            }
+        }
     }
 }
