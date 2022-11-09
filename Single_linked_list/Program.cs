471a011ac20df9b11b851d532a6874560e5897c5
ACER <ACER@DESKTOP-QH5VR0V>
2022-11-09 17:05:46 +0700

    add method Node in list

--- Single_linked_list/Program.cs
+++ Single_linked_list/Program.cs
@@ -15,5 +15,33 @@ namespace single_linked_list
         {
             START = null;
         }
+        public void addNode()/*Method untuk menambahkan sebuah Node ke dalam list*/
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            if (START == null || nim <= START.noMhs) /*Node ditambahkan sebagai Node pe*/
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+
+            /*Menentukan lokasi node baru didalam list*/
+
+            
+        }
     }
 }
