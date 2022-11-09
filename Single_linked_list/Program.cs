46637642a89bf277ccf43d7e6e583eec58eafb53
ACER <ACER@DESKTOP-QH5VR0V>
2022-11-09 18:56:50 +0700

    add fungtion switch case

--- Single_linked_list/Program.cs
+++ Single_linked_list/Program.cs
@@ -92,7 +92,7 @@ namespace single_linked_list
         /*Method untuk Transverse/mengunjungi dan membaca isi list*/
         public void transverse()
         {
-            if (listEmpty())
+            if (ListEmpty())
                 Console.WriteLine("\nList kosong. \n");
             else
             {
@@ -100,6 +100,95 @@ namespace single_linked_list
                 Node currentNode;
                 for (currentNode = START; currentNode != null; currentNode = currentNode.next)
                     Console.Write(currentNode.noMhs + " " + currentNode.nama + "\n");
+            }
+        }
+        public bool ListEmpty()
+        {
+            if (START == null)
+                return true;
+            else
+                return false;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                List obj = new List();
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("\nMenu");
+                        Console.WriteLine("1. Menambahkan data kedalam list");
+                        Console.WriteLine("2. Menghapus data dari dalam list");
+                        Console.WriteLine("3. Melihat semua data dari dalam list");
+                        Console.WriteLine("4. Mencari sebuah data didalam list");
+                        Console.WriteLine("5. Exit");
+                        Console.Write("\nMasukkan pilihan anda (1-5): ");
+                        char ch = Convert.ToChar(Console.ReadKey());
+                        switch (ch)
+                        {
+                            case '1':
+                                {
+                                    obj.addNode();
+                                }
+                                break;
+                            case '2':
+                                {
+                                    if (obj.ListEmpty())
+                                    {
+                                        Console.WriteLine("\nList Kosong");
+                                        break;
+                                    }
+                                    Console.Write("\nMasukkan nomor mahasiswa yang akan dihapus: ");
+                                    int nim = Convert.ToInt32(Console.ReadKey());
+                                    Console.WriteLine();
+                                    if (obj.delNode(nim) == false)
+                                        Console.WriteLine("\nData tidak ditemukan.");
+                                    else
+                                        Console.WriteLine("Data dengan nomor mahasiswa " + nim + " dihapus ");
+                                }
+                                break;
+                            case '3':
+                                {
+                                    obj.transverse();
+                                }
+                                break;
+                            case '4':
+                                {
+                                    if (obj.ListEmpty() == true)
+                                    {
+                                        Console.WriteLine("\nList Kosong :");
+                                        break;
+                                    }
+                                    Node previous, current;
+                                    previous = current = null;
+                                    Console.Write("\nMasukkan nomor mahasiswa yang akan dicari: ");
+                                    int num = Convert.ToInt32(Console.ReadLine());
+                                    if (obj.Search(num, ref previous, ref current) == false)
+                                        Console.WriteLine("\nData tidak ditemukan.");
+                                    else
+                                    {
+                                        Console.WriteLine("\nData ketemu");
+                                        Console.WriteLine("\nNomor Mahasiswa: " + current.noMhs);
+                                        Console.WriteLine("\nNama: " + current.nama);
+                                    }
+                                }
+                                break;
+                            case '5':
+                                return;
+                            default:
+                                {
+                                    Console.WriteLine("\nPilihan tidak valid");
+                                    break;
+                                }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("\nCheck nilai yang anda masukkan.");
+                    }
+                }
             }
         }
     }
